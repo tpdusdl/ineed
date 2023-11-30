354ea96ead92988b7290ef37c5f349e7f6d2d5c3
tpdusdl <74286434+tpdusdl@users.noreply.github.com>
2023-11-30 09:54:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import { Routes, Route} from 'react-router-dom';
 import Main from './Main';
 import Login from './Login';
-
+import Mypage from './Mypage'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -21,6 +21,7 @@ export default function App() {
       
             <Route path="/" element={<Login />}></Route>
             <Route path="/user/main" element={<Main />}></Route>
+            <Route path="/user/mypage" element={<Mypage />}></Route>
           </Routes>
           </Router>
       </div>
