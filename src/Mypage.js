7877e755726a8a7abff87cfe27b2a5abea82ae38
tpdusdl <74286434+tpdusdl@users.noreply.github.com>
2023-11-30 09:52:44 +0900

    Add files via upload

---
+++ src/Mypage.js
@@ -0,0 +1,17 @@
+// Mypage.js
+import "../src/Mypage.css";
+import React from 'react';
+
+
+const Mypage = () => {
+  return (
+    <div>
+     dd
+    </div>
+  );
+};
+
+<div className="navbar"></div>
+
+
+export default Mypage;
